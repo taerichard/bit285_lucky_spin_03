2b676abe5d02a760839dc4b34bc8e51ccd0bdf30
Brian Bansenauer <brianb@cascadia.edu>
2020-01-15 14:41:25 -0800

    updates TODOs

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LuckySpin.Controllers
@@ -10,23 +7,19 @@ namespace LuckySpin.Controllers
     {
         Random random = new Random();
 
-        //TODO: Update the Conroller to receive the Luck number from the Route
-        public IActionResult Index()
+        //The Conroller to receive the Luck number from the Route
+        public IActionResult Index(int luck = 7) //Default value for luck is 7
         {
             int a = random.Next(1, 10);
             int b = random.Next(1, 10);
             int c = random.Next(1, 10);
 
-            if (a == 7 || b == 7 || c == 7)
-                ViewBag.Display = "block";
-            else
-                ViewBag.Display = "none";
-
-
+            // Load up the ViewBag for use by the Spinner View "Index.cshtml"
+            ViewBag.ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
             ViewBag.A = a;
             ViewBag.B = b;
             ViewBag.C = c;
-
+            ViewBag.luck = luck;
 
             return View();
         }
